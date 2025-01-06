d242d207d604df58a446996eb43dc008cc1c6109
Trapo6x7 <matoufdanslezoo@gmail.com>
2025-01-06 16:30:30 +0100

    ADD:ARTICLES

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="./css/output.css">
 </head>
 
-<body class="font-inter">
+<body class="bg-off-white font-inter">
 
     <header>
         <nav class="bg-primary-yellow flex justify-between items-center p-4 w-auto">
@@ -21,7 +21,7 @@
 
             <form action="" method="get" class="flex w-6/12 justify-center">
                 <div>
-                    <input id="example" type="search" name="search" class="rounded-md w-96" />
+                    <input id="example" type="search" name="search" placeholder="Rechercher..." class="rounded-md w-96" />
                 </div>
             </form>
 
@@ -36,16 +36,16 @@
                     <img src="./asset/iconshop" alt="" class="w-12">
                 </a>
             </div>
-     </nav>
-            <nav class="p-2 flex justify-around bg-secondary-gray">
-                
-                <a href="" alt="" class="text-off-white hover:text-primary-purple">Tous les produits</a>
-                <a href="" alt="" class="text-off-white hover:text-primary-purple">Idées lecture</a>
-                <a href="" alt="" class="text-off-white hover:text-primary-purple">Notre selection</a>
-                <a href="" alt="" class="text-off-white hover:text-primary-purple">Promos</a>
-                <a href="" alt="" class="text-off-white hover:text-primary-purple">Meilleures ventes</a>
-                <a href="" alt="" class="text-off-white hover:text-primary-purple">Coup de coeur</a>
-                <a href="" alt="" class="text-off-white hover:text-primary-purple">Aide</a>
+        </nav>
+        <nav class="p-2 flex justify-around bg-secondary-gray">
+
+            <a href="" alt="" class="text-off-white hover:text-primary-purple">Tous les produits</a>
+            <a href="" alt="" class="text-off-white hover:text-primary-purple">Idées lecture</a>
+            <a href="" alt="" class="text-off-white hover:text-primary-purple">Notre selection</a>
+            <a href="" alt="" class="text-off-white hover:text-primary-purple">Promos</a>
+            <a href="" alt="" class="text-off-white hover:text-primary-purple">Meilleures ventes</a>
+            <a href="" alt="" class="text-off-white hover:text-primary-purple">Coup de coeur</a>
+            <a href="" alt="" class="text-off-white hover:text-primary-purple">Aide</a>
 
         </nav>
     </header>
@@ -54,26 +54,80 @@
 
         <section>
             <article>
-<--caroussel
+                <--caroussel
+                    </article>
+        </section>
+
+        <section class="bg-primary-yellow p-10 flex gap-5 justify-between">
+
+            <article class="bg-off-white rounded-md flex flex-col p-5 w-1/4 gap-3">
+                <div class="flex justify-end">
+                    <img src="./asset/coeuricon.png" alt="" id="like" class="w-4">
+                </div>
+
+                <div class="flex justify-center">
+                    <img src="./asset/cover/onepiece.jpg" alt="" id="cover" class="h-72 rounded-md">
+                </div>
+
+                <div class="flex flex-col">
+                    <h3 id="auteur" class="text-sm font-extralight">Auteur</h3>
+                    <h2 id="titre" class="text-lg font-extrabold text-secondary-gray">Titre</h2>
+                    <p id="price" class="text-md font-bold text-primary-purple">Prix</p>
+                </div>
+
+                <div class="flex justify-center text-sm">
+                    <a href="" alt="" class="text-off-white bg-secondary-gray p-2 rounded-md">Ajouter au panier</a>
+                </div>
+            </article>
+
+        </section>
+
+        <section class="flex justify-around">
+            <article class="text-primary-purple w-1/3 flex flex-col items-center justify-center p-5 gap-2">
+                <img src="./asset/iconsearch.png" alt="" class="w-20">
+                <h2 class="font-bold text-md">Facilité d'accès et de recherche</h2>
+                <p class="font-light text-sm text-center">Les applications spécialisées
+                    permettent de rechercher
+                    rapidement des livres en fonction
+                    de critères précis.</p>
+            </article>
+            <article class="text-primary-purple w-1/3 flex flex-col items-center justify-center p-5 gap-2">
+                <img src="./asset/iconcomuno.png" alt="" class="w-20">
+                <h2 class="font-bold text-md">Transaction simplifiée et sécurisée</h2>
+                <p class="font-light text-sm text-center">Vous avez accès à une vaste
+                    communauté d'acheteurs et de vendeurs,
+                    ce qui augmente les chances de
+                    trouver des livres rares ou spécifiques.</p>
+            </article>
+            <article class="text-primary-purple w-1/3 flex flex-col items-center justify-center p-5 gap-2">
+                <img src="./asset/iconhalass.png" alt="" class="w-20">
+                <h2 class="font-bold text-md">Large communauté et choix varié</h2>
+                <p class="font-light text-sm text-center"> Les plateformes offrent des systèmes
+                    de paiement sécurisés et des fonctionnalités
+                    de messagerie intégrées.</p>
             </article>
         </section>
 
-        <section>
-            <article>
+        <section class="bg-primary-yellow p-10 flex gap-5 justify-between">
 
+            <article class="bg-off-white rounded-md flex flex-col p-5 w-1/3 gap-3">
+
+                <div class="flex justify-center">
+                    <img src="./asset/photos/actu-visuel.jpg" alt="" id="cover" class="h-auto rounded-md">
+                </div>
+
+                <div class="flex flex-col">
+                    <h3 class="text-md font-bold">Titre Article</h3>
+                    <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolorce magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...</p>
+                </div>
+
+                <div class="flex justify-end text-sm">
+                    <a href="" alt="" class="text-off-white bg-secondary-gray p-2 w-1/2 text-center rounded-md">Lire l'article</a>
+                </div>
             </article>
-        </section>
 
-        <section>
-            <article>
-
-            </article>
-        </section>
-
-        <section>
-            <article>
-
-            </article>
         </section>
 
         <section>
