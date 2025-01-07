6619c7e18a6bfc6bf5219a8676923e6e75590645
Trapo6x7 <matoufdanslezoo@gmail.com>
2025-01-07 11:26:25 +0100

    ADD:HOMEPAGE

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
     <link rel="stylesheet" href="./css/output.css">
 </head>
 
-<body class="bg-off-white font-inter">
+<body class="bg-off-white font-inter w-screen">
 
     <header>
         <nav class="bg-primary-yellow flex justify-between items-center p-4 w-auto">
 
             <div class="flex w-3/12 justify-start">
                 <a href="" alt="">
-                    <img src="./asset/logobookmarket.png" alt="" class="w-12">
+                    <img src="./asset/logobookmarket.png" alt="logobookmarket" class="w-12">
                 </a>
             </div>
 
@@ -27,13 +27,13 @@
 
             <div class="flex w-3/12 gap-1 right-0 justify-end">
                 <a href="" alt="">
-                    <img src="./asset/iconuser.png" alt="" class="w-12">
+                    <img src="./asset/iconuser.png" alt="user" class="w-12">
                 </a>
                 <a href="" alt="">
-                    <img src="./asset/iconcontact.png" alt="" class="w-12">
+                    <img src="./asset/iconcontact.png" alt="contact" class="w-12">
                 </a>
                 <a href="" alt="">
-                    <img src="./asset/iconshop" alt="" class="w-12">
+                    <img src="./asset/iconshop" alt="shop" class="w-12">
                 </a>
             </div>
         </nav>
@@ -83,7 +83,7 @@
         </section>
 
         <section class="flex justify-around">
-            <article class="text-primary-purple w-1/3 flex flex-col items-center justify-center p-5 gap-2">
+            <article class="text-primary-purple w-1/3 flex flex-col items-center justify-center p-10 gap-2">
                 <img src="./asset/iconsearch.png" alt="" class="w-20">
                 <h2 class="font-bold text-md">Facilité d'accès et de recherche</h2>
                 <p class="font-light text-sm text-center">Les applications spécialisées
@@ -130,19 +130,95 @@
 
         </section>
 
-        <section>
-            <article>
-
+        <section class="bg-primary-purple relative">
+            <article class="px-28 py-16 flex flex-col gap-3 text-end">
+                <h2 class="text-8xl font-bold text-off-white">Offrez une nouvelle vie à vos livres</h2>
+                <p class="text-3xl text-off-white">Estimez vos livres avec notre application.</p>
+                <div class="flex justify-end mt-8 gap-8">
+                    <img src="./asset/logoapp/logogooglestore.png" alt="googleplay" class="w-1/5 h-auto">
+                    <img src="./asset/logoapp/logoapplestore.png" alt="appstore" class="w-1/5 h-auto">
+                </div>
+                <img src="./asset/photos/mockupappli.png" alt="screenshot" class="absolute h-[78%] mt-16">
             </article>
         </section>
 
         <section>
-            <article>
+            <article class="bg-off-white text-secondary-gray p-10 flex flex-col gap-3 text-sm">
+                <h2 class="font-bold">Pourquoi acheter des livres d'occasion ?</h2>
 
+                <p>Faire le choix de l'occasion permet d'acheter des livres pas chers. Leur prix est bien plus bas que les livres neufs et ils sont le plus souvent en très bon état. L'état des livres que nous vendons est
+                    scrupuleusement vérifié et répond à une charte de qualité précise. Les livres d'occasion ont un très bon impact sur l'environnement. Saviez-vous que plus de 142 millions de livres sont pilonnés
+                    chaque année en France ? Pourtant, la plupart sont encore en état d'être lus. Pour mettre fin à cette aberration, faisons de l'occasion une norme !</p>
+
+                <h2 class="font-bold">Pourquoi acheter des livres sur BookMarket ?</h2>
+
+                <p> En achetant sur la boutique en ligne de BookMarket, vous faites le choix d'une boutique 100% française qui participe à l'économie circulaire. En effet, tous les livres auxquels nous offrons
+                    une seconde vie ont été collectés en France. En achetant des livres d'occasion sur notre site, vous bénéficiez de la livraison gratuite et les commandes sont traitées et expédiées en 24h. Pour permettre à chacun d'acheter des livres pas chers, nous faisons notre maximum pour proposer les meilleurs prix.</p>
+
+                <h2 class="font-bold">Qui sommes-nous ?</h2>
+
+                <p>BookMarket est la plateforme pour vendre et acheter des livres d'occasion. Nous permettons à chacun d'offrir une seconde vie à ses livres. Notre mission est de revaloriser les livres
+                    d’occasion en proposant à chacun un service simple et transparent. Nous voulons proposer une alternative crédible à l'achat de biens neufs avec une vision claire et ambitieuse : faire de l'occasion
+                    une norme.</p>
+
+                <h2 class="font-bold">Comment vendre ses livres ?</h2>
+
+                <p>La Bourse aux Livres n'est pas simplement une boutique en ligne, nous sommes avant tout un dépôt-vente. Nous vous proposons de nous confier vos livres et nous les vendons à votre place. Il s'agit
+                    de la meilleure solution pour vendre ses livres simplement et au meilleur prix. En téléchargeant notre application pour smartphone, vous pouvez obtenir une estimation immédiate de combien vous
+                    rapporteront vos livres. Il vous suffit ensuite de nous les envoyer et nous nous occupons de tout de A à Z. Vous n'avez plus qu'à attendre vos gains !</p>
             </article>
         </section>
 
-        <footer>
+        <footer class="bg-secondary-gray p-10 text-off-white flex justify-between">
+
+            <div class="flex flex-col gap-24">
+                <div class="text-lg">
+                    <h2>BookMarket</h2>
+                </div>
+                <div class="flex gap-2 text-xs">
+                    <a href="" alt="">
+                        <p>Tous les produits</p>
+                    </a>
+                    <p>/</p>
+                    <a href="" alt="">
+                        <p>Idées lecture</p>
+                    </a>
+                    <p>/</p>
+                    <a href="" alt="">
+                        <p>Notre sélection</p>
+                    </a>
+                    <p>/</p>
+
+                    <a href="" alt="">
+                        <p>Promo</p>
+                    </a>
+                    <p>/</p>
+                    <a href="" alt="">
+                        <p>Meilleures ventes</p>
+                    </a>
+                    <p>/</p>
+                    <a href="" alt="">
+                        <p>Coup de coeur</p>
+                    </a>
+                    <p>/</p>
+                    <a href="" alt="">
+                        <p>Aide</p>
+                    </a>
+                </div>
+            </div>
+
+            <div class="flex flex-col justify-end items-end gap-5">
+                <div class="text-end">
+                    <p class="text-xs">Contact us</p>
+                    <p class="text-md">+1 999 888-76-54</p>
+                </div>
+
+                <div class="text-end">
+                    <p class="text-xs">Email</p>
+                    <p class="text-md">hello@bookmarket.com</p>
+                </div>
+            </div>
+
 
         </footer>
 
